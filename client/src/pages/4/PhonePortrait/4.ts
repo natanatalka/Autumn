d681b000dda554a4e563c4e2c899864b4fc03820
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:12:43 +0300

    [Studio] Add screen "4"

---
+++ client/src/pages/4/PhonePortrait/4.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-4-phoneportrait',
+  templateUrl: '4.html'
+})
+export class 4_PhonePortrait extends Screen {
+}

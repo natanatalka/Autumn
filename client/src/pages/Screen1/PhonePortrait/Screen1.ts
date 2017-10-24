e1978486d25515047bdd9db3873ea1fa03a11eb8
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:17:34 +0300

    [Studio] Add screen "screen1"

---
+++ client/src/pages/Screen1/PhonePortrait/Screen1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-screen1-phoneportrait',
+  templateUrl: 'Screen1.html'
+})
+export class Screen1_PhonePortrait extends Screen {
+}

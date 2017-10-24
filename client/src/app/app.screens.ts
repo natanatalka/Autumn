98cffa84105220b02c1a623927c0f6a117785ce9
test <test@test.com>
2017-10-24 15:15:11 +0300

    [Studio] Add screen "Test1"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -3,11 +3,13 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { September_PhonePortrait } from '../pages/September/PhonePortrait/September';
 import { November_PhonePortrait } from '../pages/November/PhonePortrait/November';
+import { Test1_PhonePortrait } from '../pages/Test1/PhonePortrait/Test1';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     September_PhonePortrait,
-    November_PhonePortrait
+    November_PhonePortrait,
+    Test1_PhonePortrait
   ];
   static mapping = {
     'September': {
@@ -15,6 +17,9 @@ export class Screens {
     },
     'November': {
       PhonePortrait: November_PhonePortrait
+    },
+    'Test1': {
+      PhonePortrait: Test1_PhonePortrait
     }
   }
 }

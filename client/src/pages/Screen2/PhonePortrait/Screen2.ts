fca53d73976865119fa680fd1dcf2f9d1af812e3
test <test@test.com>
2017-10-24 15:18:10 +0300

    [Studio] Delete screen "Screen2"

--- client/src/pages/Screen2/PhonePortrait/Screen2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-screen2-phoneportrait',
-  templateUrl: 'Screen2.html'
-})
-export class Screen2_PhonePortrait extends Screen {
-}

06d96ca42c0d7fcc42dd4460e74ac35ec538dc27
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:12:51 +0300

    [Studio] Delete screen "4"

--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,2 @@
-export * from './4/index';
 export * from './November/index';
 export * from './September/index';
--- client/src/pages/4/PhonePortrait/4.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-4-phoneportrait',
-  templateUrl: '4.html'
-})
-export class 4_PhonePortrait extends Screen {
-}

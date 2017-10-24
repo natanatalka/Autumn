167439d9926f6fd5ff932e9d2015c99c15a54e99
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:26:39 +0300

    [Studio] Add screen "222"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -3,11 +3,13 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { September_PhonePortrait } from '../pages/September/PhonePortrait/September';
 import { November_PhonePortrait } from '../pages/November/PhonePortrait/November';
+import { 222 _PhonePortrait } from '../pages/222/PhonePortrait/222';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     September_PhonePortrait,
-    November_PhonePortrait
+    November_PhonePortrait,
+    222 _PhonePortrait
   ];
   static mapping = {
     'September': {
@@ -15,6 +17,9 @@ export class Screens {
     },
     'November': {
       PhonePortrait: November_PhonePortrait
+    },
+    '222': {
+      PhonePortrait: 222 _PhonePortrait
     }
   }
 }

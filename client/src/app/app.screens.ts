5a4d7f407e33dda03d9e600b4db20a89c6ec1025
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:16:58 +0300

    [Studio] Delete screen "Screen1"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -3,13 +3,11 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { September_PhonePortrait } from '../pages/September/PhonePortrait/September';
 import { November_PhonePortrait } from '../pages/November/PhonePortrait/November';
-import { Screen1_PhonePortrait } from '../pages/Screen1/PhonePortrait/Screen1';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     September_PhonePortrait,
-    November_PhonePortrait,
-    Screen1_PhonePortrait
+    November_PhonePortrait
   ];
   static mapping = {
     'September': {
@@ -17,9 +15,6 @@ export class Screens {
     },
     'November': {
       PhonePortrait: November_PhonePortrait
-    },
-    'Screen1': {
-      PhonePortrait: Screen1_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,2 @@
 export * from './November/index';
-export * from './Screen1/index';
 export * from './September/index';

92f349ae62ce1b517c35e98ee6e8547ee89e9f86
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 17:30:10 +0300

    [Studio] Add screen "Login"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -6,6 +6,7 @@ import { November_PhonePortrait } from '../pages/November/PhonePortrait/November
 import { Nata2_PhonePortrait } from '../pages/Nata2/PhonePortrait/Nata2';
 import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
 import { Blank1_PhonePortrait } from '../pages/Blank1/PhonePortrait/Blank1';
+import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
@@ -13,7 +14,8 @@ export class Screens {
     November_PhonePortrait,
     Nata2_PhonePortrait,
     Blank_PhonePortrait,
-    Blank1_PhonePortrait
+    Blank1_PhonePortrait,
+    Login_PhonePortrait
   ];
   static mapping = {
     'September': {
@@ -30,6 +32,9 @@ export class Screens {
     },
     'Blank1': {
       PhonePortrait: Blank1_PhonePortrait
+    },
+    'Login': {
+      PhonePortrait: Login_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,5 +1,6 @@
 export * from './Blank/index';
 export * from './Blank1/index';
+export * from './Login/index';
 export * from './Nata2/index';
 export * from './November/index';
 export * from './September/index';

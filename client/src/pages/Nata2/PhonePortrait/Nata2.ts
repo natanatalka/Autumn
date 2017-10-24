48e1de44aa993955bd4489e3673237a954dae469
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:28:11 +0300

    [Studio] Add screen "nata2"

---
+++ client/src/pages/Nata2/PhonePortrait/Nata2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-nata2-phoneportrait',
+  templateUrl: 'Nata2.html'
+})
+export class Nata2_PhonePortrait extends Screen {
+}

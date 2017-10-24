d8b950229e3099d1eb53f81f2daa88edb62389a1
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:08:29 +0300

    [Studio] Add screen "September"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,16 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
-import { MenuComponent } from "./menu/menu.component";
-
+import { MenuComponent } from './menu/menu.component';
+import { September_PhonePortrait } from '../pages/September/PhonePortrait/September';
 export class Screens {
-
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    September_PhonePortrait
   ];
-
   static mapping = {
+    'September': {
+      PhonePortrait: September_PhonePortrait
+    }
   }
-
 }
-
-

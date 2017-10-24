8c06a5b476c0f68874a66e7655d0ce2b02952a23
test <test@test.com>
2017-10-24 17:29:08 +0300

    [Studio] Add screen "Blank"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -4,12 +4,14 @@ import { MenuComponent } from './menu/menu.component';
 import { September_PhonePortrait } from '../pages/September/PhonePortrait/September';
 import { November_PhonePortrait } from '../pages/November/PhonePortrait/November';
 import { Nata2_PhonePortrait } from '../pages/Nata2/PhonePortrait/Nata2';
+import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     September_PhonePortrait,
     November_PhonePortrait,
-    Nata2_PhonePortrait
+    Nata2_PhonePortrait,
+    Blank_PhonePortrait
   ];
   static mapping = {
     'September': {
@@ -20,6 +22,9 @@ export class Screens {
     },
     'Nata2': {
       PhonePortrait: Nata2_PhonePortrait
+    },
+    'Blank': {
+      PhonePortrait: Blank_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,4 @@
+export * from './Blank/index';
 export * from './Nata2/index';
 export * from './November/index';
 export * from './September/index';
